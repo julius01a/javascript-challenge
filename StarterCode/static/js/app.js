25f52e3cf06b6ee32e2c3c95894b7393a14f8045
julius01a <66196006+julius01a@users.noreply.github.com>
2020-08-22 21:57:19 +1000

    add default opeing table

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,19 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
+// Display the whole table as opening default
+
+tableData.forEach((record) => {
+    console.log(record);
+    var row = tbody.append('tr');
+
+    Object.entries(record).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
 
 
 
@@ -12,14 +25,14 @@ var button = d3.select("#filter-btn");
 var form = d3.select("#datetime");
 // Create event handlers 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+form.on("submit", runEnter);
 // Complete the event handler function for the form
 function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
   
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var inputElement = form.select("#datetime");
   
   // Get the value property of the input element
   var userInput = inputElement.property("value");  
@@ -32,4 +45,27 @@ function runEnter() {
   
   //checking search records, worked, pass to next step
   //console.log(matchingCase); 
+  // Show filtered results only in main table
+  if (matchingCase.length == 0) {
+    // Check where there are 0 matches
+    // console.log(`No results for date ${userInput}`);
+    tbody.html("");
+    tbody.text(`There are no results for the date you entered - ${userInput}`);
+} 
+else {
+    tbody.html("");
+    matchingCase.forEach((report) => {
+    var row = tbody.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+      // Check entries
+        // console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+  });
+};    
+
+
+
 };
